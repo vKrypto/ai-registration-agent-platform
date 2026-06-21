b576a8e4962e85f5a0e71a0ed7b7bc3bed778fc6
Ashutosh verma <vkrypto@gmail.com>
2026-06-21 09:11:39 +0530

    Add Terraform Azure provider configuration

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
